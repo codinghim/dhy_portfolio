21cc5778285a41db8032812bfbc0675220f58994
codinghim <coding0703@gmail.com>
2022-05-20 15:11:53 -0400

    portfolio section v0.1

---
+++ components/Navbar.js
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+import Styles from './styles/navbar.module.scss'
+
+const Navbar = () => {
+    return(
+        <>
+        {/* Navbar Logo */}
+        <Link href="/">
+            <a className={Styles.nav_logo}>Daniel Himchan Yun</a>
+        </Link>
+
+        {/* Navbar */}
+        <nav id="navbar" className={Styles.nav}>
+            <div className={Styles.navLink}></div>
+            <div className={Styles.navLink_container}>
+                <Link href="/">
+                    <a className={Styles.navLink}>About</a>
+                </Link>
+                <Link href="/">
+                    <a className={Styles.navLink}>Styles</a>
+                </Link>
+                <Link href="/">
+                    <a className={Styles.navLink}>Services</a>
+                </Link>
+                <a href="#contactus" className={Styles.navLink}>Contact</a>
+            </div>         
+        </nav>
+
+        
+        </>
+    )
+}
+
+export default Navbar;
