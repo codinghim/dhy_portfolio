24f6dcc3f2b4339d087424834c00eab6cf9ca305
codinghim <coding0703@gmail.com>
2022-05-21 15:08:15 -0400

    intro section v0.1

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,47 @@
 import Link from 'next/link'
+import { useEffect } from 'react';
 import Styles from './styles/navbar.module.scss'
 
 const Navbar = () => {
+    useEffect(() =>{
+        let prevScrollPosition = 0;
+        window.onscroll= () =>{
+            let currentScrollPosition = document.documentElement.scrollTop
+            let px = window.innerHeight*0.12 //12vh in px
+            let offsetHeight = document.getElementById('navbar').offsetHeight
+            //if scrolltop is more than 12vh show navbar
+            if(currentScrollPosition>prevScrollPosition){
+                document.getElementById('navbar').style.transform = "none";
+            }else{
+                document.getElementById('navbar').style.transform = "none";
+                document.getElementById('navbar').style.transform = "translateY(-12vh)";
+            }
+            
+            // if(currentScrollPosition > prevScrollPosition){
+            //     //hide navbar when scrolling down
+            //     if( document.documentElement.clientWidth < 540){ 
+            //         //on mobile of width less than 540px
+            //         document.getElementById('navbar_m').style.transform = "translateY(-8vh)";
+            //     }else if( document.documentElement.clientWidth < 768 ){ 
+            //         //on mobile of width greater than 540px
+            //         document.getElementById('navbar').style.transform = "translateY(-8vh)"; //change inside to the height of navbar located in /styles/lib/var.scss
+            //     }else{ 
+            //         //on larger screen
+            //         document.getElementById('navbar').style.transform = "translateY(-12vh)"; //change inside to the height of navbar located in /styles/lib/var.scss
+            //     }
+            // }else{
+            //     //hide navbar when scrolling down
+            //     if( document.documentElement.clientWidth < 540){
+            //         document.getElementById('navbar_m').style.transform = "none";
+            //     }else{
+            //         document.getElementById('navbar').style.transform = "none";
+            //     }
+                
+            // }
+            prevScrollPosition = document.documentElement.scrollTop
+        }
+    })
+    
     return(
         <>
         {/* Navbar Logo */}
@@ -14,13 +54,13 @@ const Navbar = () => {
             <div className={Styles.navLink}></div>
             <div className={Styles.navLink_container}>
                 <Link href="/">
-                    <a className={Styles.navLink}>About</a>
+                    <a className={Styles.navLink}>Menu1</a>
                 </Link>
                 <Link href="/">
-                    <a className={Styles.navLink}>Styles</a>
+                    <a className={Styles.navLink}>Menu2</a>
                 </Link>
                 <Link href="/">
-                    <a className={Styles.navLink}>Services</a>
+                    <a className={Styles.navLink}>Menu3</a>
                 </Link>
                 <a href="#contactus" className={Styles.navLink}>Contact</a>
             </div>         
--- pages/index.js
+++ pages/index.js
@@ -13,26 +13,36 @@ export default function Home() {
         </Head>
         <Layout>
             <section className={Styles.intro_section}>
-                <div className={Styles.main_image_container}>
-                    Hello
+                <div>
+                    <p className={Styles.intro_text}>Hi, I'm Daniel. <br />I am a web developer.</p>
                 </div>
-                <div className={Styles.main_image_container}>
-                    Hello
+            </section>
+            <section className={Styles.about_section}>
+                <div className={Styles.about_text_container}>
+                    <div className={Styles.arrow_img_container}>
+                    <img className={Styles.arrow_img} src="/images/comic_thumbnail.jpg"></img>
+                    </div>
+                    <p className={Styles.about_text}>I am a web developer. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
-                <div className={Styles.main_image_container}>
-                    Hello
+            </section>
+            <section className={Styles.skills_section}>
+                <div>
+                    <p>Full Stack Devopers</p>
+                    <p>Languages</p>
+                    <p>Python, JavaScript, Java</p>
+                    <p>Frameworks</p>
+                    <p>MERN Stack, Jquery, Sass, NextJS</p>
+                    <p>Database</p>
+                    <p>MySQL, MongoDB</p>
                 </div>
-                <div className={Styles.main_image_container}>
-                    Hello
-                </div>
-                <div className={Styles.main_image_container}>
-                    Hello
-                </div>
-                <div className={Styles.main_image_container}>
-                    Hello
-                </div>
-                <div className={Styles.main_image_container}>
-                    Hello
+            </section>
+            <section className={Styles.experience_section}>
+                <div>
+                    <p>Experience</p>
+                    <p>Magi Web Production | 2020 - 2021</p>
+                    <p>Co-CEO & Software Developer</p>
+                    <p>A start-up company which provides web solutions to businesses</p>
+                    <p>https://company-website-coral.vercel.app/</p>
                 </div>
 
             </section>
@@ -61,6 +71,34 @@ export default function Home() {
                         </div>
                         
                     </a>
+                    <div className={Styles.portfolio_grid_item}>
+                        <div className={Styles.portfolio_image_container}>
+                            <Image src={`/images/cyber_security_thumbnail.jpg`} width={350} height={250} objectFit="fill"/>
+                            <div className={Styles.portfolio_grid_item_hover}>
+                                <div className={Styles.portfolio_grid_item_hover_title}>
+                                    View
+                                </div>
+                            </div>
+                        </div>
+                        <div className={Styles.portfolio_image_title_container}>
+                            <p className={Styles.portfolio_image_title}>Project project 2</p>
+                            <p className={Styles.portfolio_image_subtitle}>project and description</p>
+                        </div>
+                    </div>
+                    <div className={Styles.portfolio_grid_item}>
+                        <div className={Styles.portfolio_image_container}>
+                            <Image src={`/images/cyber_security_thumbnail.jpg`} width={350} height={250} objectFit="fill"/>
+                            <div className={Styles.portfolio_grid_item_hover}>
+                                <div className={Styles.portfolio_grid_item_hover_title}>
+                                    View
+                                </div>
+                            </div>
+                        </div>
+                        <div className={Styles.portfolio_image_title_container}>
+                            <p className={Styles.portfolio_image_title}>Project project 2</p>
+                            <p className={Styles.portfolio_image_subtitle}>project and description</p>
+                        </div>
+                    </div>
                     <div className={Styles.portfolio_grid_item}>
                         <div className={Styles.portfolio_image_container}>
                             <Image src={`/images/cyber_security_thumbnail.jpg`} width={350} height={250} objectFit="fill"/>
