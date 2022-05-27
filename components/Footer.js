a290a46b5846713953c682205c26e0845d307cbc
codinghim <coding0703@gmail.com>
2022-05-27 15:52:31 -0400

    base of sections

--- components/Footer.js
+++ components/Footer.js
@@ -5,41 +5,10 @@ const Footer = () => {
     return(
         <>
             <footer id="contactus" className={Styles.footer}>
-                <div className={Styles.contact_info_container}>
-                    <div className={Styles.info_container}>
-                        <div className={Styles.info_row}>
-                            <p className={Styles.address}>address</p>
-                            <a href="https://www.google.com/maps/place/Empire+State+Building/@40.7484405,-73.9856644,15z/data=!4m5!3m4!1s0x0:0xd134e199a405a163!8m2!3d40.7484405!4d-73.9856644">
-                                <p className={Styles.address}>20 W 34th St, New York, NY 10001</p>
-                            </a>
-                            
-                        </div>
-                        <div className={Styles.info_row}>
-                            
-                            <p className={Styles.number}>call</p>
-                            <div className={Styles.number}><a href="tel:777-777-7777">777-777-7777</a></div>
-                        </div>
-                    </div>
-                    <div className={Styles.info_container}>
-                        <div className={Styles.hour_row}>
-                            <p className={Styles.hour}>Sun</p>
-                            <p className={Styles.hour}>9am - 6pm</p>
-                        </div>
-                        <div className={Styles.hour_row}>
-                            <p className={Styles.hour}>Mon-Fri</p>
-                            <p className={Styles.hour}>9am - 7pm</p>
-                        </div>
-                        <div className={Styles.hour_row}>
-                            <p className={Styles.hour}>Sat</p>
-                            <p className={Styles.hour}>9am - 8pm</p>
-                        </div>
-                    </div>
+               <div className={Styles.icons_container}>
+                    <a href="https://github.com/codinghim"><Image src={"/images/logo/GitHub-Mark.png"} width={40} height={40}/></a>
                 </div>
-                <div className={Styles.icons_container}>
-                    <a href="https://instagram.com">instagram</a>
-                    <a href="https://facebook.com">facebook</a>
-                </div>
-                <p className={Styles.copyright}>Copyright &copy; 2021 MAGI Web Production. <br />All rights reserved</p>
+                <p className={Styles.copyright}>Copyright &copy; 2022 Daniel Himchan Yun <br />All rights reserved</p>
             </footer>
         </>
     )
--- pages/index.js
+++ pages/index.js
@@ -15,36 +15,46 @@ export default function Home() {
                     <p className={Styles.intro_text}>Hi, I&#39;m Daniel. <br />I am a web developer.</p>
                 </div>
             </section>
+
+
             <section className={Styles.about_section}>
-                <div className={Styles.about_text_container}>
+                <div className={Styles.about_container}>
                     <div className={Styles.arrow_img_container}>
                         {/* <a className={Styles.arrow_down} */}
                         <Image src="/images/comic_thumbnail.jpg" layout="fill" />
                     </div>
-                    <p className={Styles.about_text}>I am a web developer. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <div className={Styles.about_text_container}>
+                        <p className={Styles.about_text}>I am a web developer. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
                 </div>
             </section>
-            <section className={Styles.skills_section}>
-                <div>
-                    <p>Full Stack Devopers</p>
-                    <p>Languages</p>
-                    <p>Python, JavaScript, Java</p>
-                    <p>Frameworks</p>
-                    <p>MERN Stack, Jquery, Sass, NextJS</p>
-                    <p>Database</p>
-                    <p>MySQL, MongoDB</p>
-                </div>
-            </section>
-            <section className={Styles.experience_section}>
-                <div>
-                    <p>Experience</p>
-                    <p>Magi Web Production | 2020 - 2021</p>
-                    <p>Co-CEO and Software Developer</p>
-                    <p>A start-up company which provides web solutions to businesses</p>
-                    <p>https://company-website-coral.vercel.app/</p>
-                </div>
 
+
+            <section className={Styles.skills_section}>
+                <div className={Styles.skills_container}>
+                    <p className={Styles.skills_title}>Skills</p>
+                    <p className={Styles.skills_header}>Languages</p>
+                    <p className={Styles.skills_text}>Python, JavaScript, Java</p>
+                    <p className={Styles.skills_header}>Frameworks</p>
+                    <p className={Styles.skills_text}>MERN Stack, Jquery, Sass, NextJS</p>
+                    <p className={Styles.skills_header}>Database</p>
+                    <p className={Styles.skills_text}>MySQL, MongoDB</p>
+                </div>
             </section>
+
+
+            <section className={Styles.experience_section}>
+                    <p className={Styles.experience_section_title}>Experience</p>
+                    <div>
+                        <Image src={'/images/logo/magi_logo.png'} width={150} height={150} objectFit="fill"/>
+                        <p className={Styles.experience_title}>Magi Web Production | 2020 - 2021</p>
+                        <p className={Styles.experience_position}>Co-CEO and Software Developer</p>
+                        <p className={Styles.experience_description}>A start-up company which provides web solutions to businesses</p>
+                        <a href="https://company-website-coral.vercel.app/" target="_blank" rel="noopener noreferrer" className={Styles.hyperlink}>Visit Website</a>
+                    </div>
+            </section>
+
+
             <section className={Styles.portfolio_section}>
                 <div className={Styles.portfolio_title_container}>
                     <p className={Styles.portfolio_title}>
@@ -55,7 +65,7 @@ export default function Home() {
                     </p>
                 </div>
                 <div className={Styles.portfolio_grid}>
-                    <a className={Styles.portfolio_grid_item} href="https://github.com/codinghim/team_white_308_thecomicverse">
+                    <a className={Styles.portfolio_grid_item} href="https://github.com/codinghim/team_white_308_thecomicverse" target="_blank" rel="noopener noreferrer">
                         <div className={Styles.portfolio_image_container}>
                             <Image src={`/images/comic_thumbnail.jpg`} width={350} height={250} objectFit="fill"/>
                             <div className={Styles.portfolio_grid_item_hover}>
@@ -70,7 +80,21 @@ export default function Home() {
                         </div>
                         
                     </a>
-                    <div className={Styles.portfolio_grid_item}>
+                    <a className={Styles.portfolio_grid_item} href="https://github.com/codinghim/company_website" target="_blank" rel="noopener noreferrer">
+                        <div className={Styles.portfolio_image_container}>
+                            <Image src={`/images/magi_thumbnail.png`} width={350} height={250} objectFit="fill"/>
+                            <div className={Styles.portfolio_grid_item_hover}>
+                                <div className={Styles.portfolio_grid_item_hover_title}>
+                                    View
+                                </div>
+                            </div>
+                        </div>
+                        <div className={Styles.portfolio_image_title_container}>
+                            <p className={Styles.portfolio_image_title}>Magi Website</p>
+                            <p className={Styles.portfolio_image_subtitle}>project and description</p>
+                        </div>
+                    </a>
+                    <a className={Styles.portfolio_grid_item} href="https://github.com/codinghim/company_website" target="_blank" rel="noopener noreferrer">
                         <div className={Styles.portfolio_image_container}>
                             <Image src={`/images/cyber_security_thumbnail.jpg`} width={350} height={250} objectFit="fill"/>
                             <div className={Styles.portfolio_grid_item_hover}>
@@ -80,11 +104,11 @@ export default function Home() {
                             </div>
                         </div>
                         <div className={Styles.portfolio_image_title_container}>
-                            <p className={Styles.portfolio_image_title}>Project project 2</p>
+                            <p className={Styles.portfolio_image_title}>Website Templates</p>
                             <p className={Styles.portfolio_image_subtitle}>project and description</p>
                         </div>
-                    </div>
-                    <div className={Styles.portfolio_grid_item}>
+                    </a>
+                    <a className={Styles.portfolio_grid_item} href="https://github.com/codinghim/company_website" target="_blank" rel="noopener noreferrer">
                         <div className={Styles.portfolio_image_container}>
                             <Image src={`/images/cyber_security_thumbnail.jpg`} width={350} height={250} objectFit="fill"/>
                             <div className={Styles.portfolio_grid_item_hover}>
@@ -94,11 +118,11 @@ export default function Home() {
                             </div>
                         </div>
                         <div className={Styles.portfolio_image_title_container}>
-                            <p className={Styles.portfolio_image_title}>Project project 2</p>
+                            <p className={Styles.portfolio_image_title}>Flash Cards</p>
                             <p className={Styles.portfolio_image_subtitle}>project and description</p>
                         </div>
-                    </div>
-                    <div className={Styles.portfolio_grid_item}>
+                    </a>
+                    <a className={Styles.portfolio_grid_item} href="https://github.com/codinghim/CSE363-Cyber-Security-Attacks" target="_blank" rel="noopener noreferrer">
                         <div className={Styles.portfolio_image_container}>
                             <Image src={`/images/cyber_security_thumbnail.jpg`} width={350} height={250} objectFit="fill"/>
                             <div className={Styles.portfolio_grid_item_hover}>
@@ -108,24 +132,10 @@ export default function Home() {
                             </div>
                         </div>
                         <div className={Styles.portfolio_image_title_container}>
-                            <p className={Styles.portfolio_image_title}>Project project 2</p>
+                            <p className={Styles.portfolio_image_title}>Cyber Security Attacks</p>
                             <p className={Styles.portfolio_image_subtitle}>project and description</p>
                         </div>
-                    </div>
-                    <div className={Styles.portfolio_grid_item}>
-                        <div className={Styles.portfolio_image_container}>
-                            <Image src={`/images/cyber_security_thumbnail.jpg`} width={350} height={250} objectFit="fill"/>
-                            <div className={Styles.portfolio_grid_item_hover}>
-                                <div className={Styles.portfolio_grid_item_hover_title}>
-                                    View
-                                </div>
-                            </div>
-                        </div>
-                        <div className={Styles.portfolio_image_title_container}>
-                            <p className={Styles.portfolio_image_title}>Project project 2</p>
-                            <p className={Styles.portfolio_image_subtitle}>project and description</p>
-                        </div>
-                    </div>
+                    </a>
                 </div>
             </section>
         </Layout>
