2677944ca0f57c6f93e2a489eec32c215b231fd3
codinghim <coding0703@gmail.com>
2022-05-30 13:46:36 -0400

    aos and section position

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import '../styles/globals.scss'
+import Aos from "aos"
+import "aos/dist/aos.css"
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(()=>{
+    Aos.init({duration:1300})
+  })
   return <Component {...pageProps} />
 }
 
--- pages/index.js
+++ pages/index.js
@@ -16,13 +16,124 @@ export default function Home() {
                 </div>
             </section>
             <section className={Styles.about_section} id="about_text">
-                <div className={Styles.about_text_container}>
+                <div className={Styles.about_container}>
                     <div className={Styles.arrow_img_container}>
                         <a className={Styles.arrow_down} href="#about_text"></a>
-                        {/* <img className={Styles.arrow_img} src="/images/comic_thumbnail.jpg"></img> */}
                     </div>
-                    <p  className={Styles.about_text}>I am a web developer. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <div className={Styles.about_text_container}>
+                        <p className={Styles.about_text}>I am a web developer who brings ideas to life through web. I like to analyze problems and like to try to find solution. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
                 </div>
+            </section>
+
+
+            <section className={Styles.portfolio_section}>
+                <div className={Styles.portfolio_title_container}>
+                    <p className={Styles.portfolio_title}>
+                        Portfolio
+                    </p>
+                    <p className={Styles.portfolio_subtitle}>
+                        Knowledge and skills
+                    </p>
+                </div>
+                <div className={Styles.portfolio_grid}>
+                    <a className={Styles.portfolio_grid_item} href="https://github.com/codinghim/team_white_308_thecomicverse" target="_blank" rel="noopener noreferrer" data-aos="fade-up">
+                        <div className={Styles.portfolio_image_container}>
+                            <Image src={`/images/comic_thumbnail.jpg`} width={350} height={250} objectFit="fill"/>
+                            <div className={Styles.portfolio_grid_item_hover}>
+                                <div className={Styles.portfolio_image_title_container}>
+                                    <p className={Styles.portfolio_image_title}>The ComicVerse</p>
+                                    <p className={Styles.portfolio_image_subtitle}>
+                                        A <em>full-stack web application</em> where users can create, view, and upload comics. <br />
+                                        <em>Project management</em> using Slack and documentation.
+                                    </p>
+                                    <p className={Styles.portfolio_image_subtitle}>
+                                        Java, Spring Framework, JQuery, MongoDB
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </a>
+                    <a className={Styles.portfolio_grid_item} href="https://github.com/codinghim/company_website" target="_blank" rel="noopener noreferrer" data-aos="fade-up" data-aos-delay="200">
+                        <div className={Styles.portfolio_image_container}>
+                            <Image src={`/images/magi_thumbnail.png`} width={350} height={250} objectFit="fill"/>
+                            <div className={Styles.portfolio_grid_item_hover}>
+                                <div className={Styles.portfolio_image_title_container}>
+                                    <p className={Styles.portfolio_image_title}>Magi Website</p>
+                                    <p className={Styles.portfolio_image_subtitle}>
+                                        A <em>web application</em> to promote services.<br />
+                                        Work closely with the team to design UI.<br />
+                                    </p>
+                                    <p className={Styles.portfolio_image_subtitle}>
+                                        ReactJS, NextJS, CSS, Sass
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                    <a className={Styles.portfolio_grid_item} href="https://github.com/codinghim/company_website" target="_blank" rel="noopener noreferrer" data-aos="fade-up" data-aos-delay="400">
+                        <div className={Styles.portfolio_image_container}>
+                            <Image src={`/images/cyber_security_thumbnail.jpg`} width={350} height={250} objectFit="fill"/>
+                            <div className={Styles.portfolio_grid_item_hover}>
+                                <div className={Styles.portfolio_image_title_container}>
+                                    <p className={Styles.portfolio_image_title}>Website Templates</p>
+                                    <p className={Styles.portfolio_image_subtitle}>project and description</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                    <a className={Styles.portfolio_grid_item} href="https://github.com/codinghim/company_website" target="_blank" rel="noopener noreferrer" data-aos="fade-up">
+                        <div className={Styles.portfolio_image_container}>
+                            <Image src={`/images/cyber_security_thumbnail.jpg`} width={350} height={250} objectFit="fill"/>
+                            <div className={Styles.portfolio_grid_item_hover}>
+                                <div className={Styles.portfolio_image_title_container}>
+                                    <p className={Styles.portfolio_image_title}>Flash Cards</p>
+                                    <p className={Styles.portfolio_image_subtitle}>
+                                        A <em>full-stck web application</em> where users can enter a set of words and use as an aid to memorize words.
+                                    </p>
+                                    <p className={Styles.portfolio_image_subtitle}>
+                                        Python, Flask
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                    <a className={Styles.portfolio_grid_item} href="https://github.com/codinghim/CSE363-Cyber-Security-Attacks" target="_blank" rel="noopener noreferrer" data-aos="fade-up" data-aos-delay="200">
+                        <div className={Styles.portfolio_image_container}>
+                            <Image src={`/images/cyber_security_thumbnail.jpg`} width={350} height={250} objectFit="fill"/>
+                            <div className={Styles.portfolio_grid_item_hover}>
+                                <div className={Styles.portfolio_image_title_container}>
+                                    <p className={Styles.portfolio_image_title}>Cyber Security Attacks</p>
+                                    <p className={Styles.portfolio_image_subtitle}>project and description</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </section>
+            
+
+            <section className={Styles.experience_section}>
+                    <p className={Styles.experience_section_title}>Experience</p>
+                    <p className={Styles.experience_section_subtitle}>Professional experience</p>
+                    <div className={Styles.experience_container}>
+                        <Image src={'/images/logo/magi_logo.png'} width={150} height={150} objectFit="fill"/>
+                        <p className={Styles.experience_title}>Magi Web Production | 2020 - 2021</p>
+                        <p className={Styles.experience_position}>Co-CEO and Software Developer</p>
+                        <p className={Styles.experience_description}>&bull; A start-up company which provides web solutions to businesses.<br />
+                            &bull; Planned and managed project development as a team.<br />
+                            &bull; implemented simple and attractive responsive web design.</p>
+                        <a href="https://company-website-coral.vercel.app/" target="_blank" rel="noopener noreferrer" className={Styles.hyperlink}>Visit Website</a>
+                    </div>
+                    <div className={Styles.experience_divider_container}>
+                        <div className={Styles.experience_divider}></div>
+                    </div>
+                    <div className={Styles.experience_container}>
+                        <p className={Styles.experience_goal_logo}>SUCCESS</p>
+                        <p className={Styles.experience_title}>Way to Success | Today -</p>
+                        <p className={Styles.experience_position}>Software Developer & A Good Person</p>
+                    </div>
             </section>
 
 
@@ -38,100 +149,7 @@ export default function Home() {
                 </div>
             </section>
 
-
-            <section className={Styles.experience_section}>
-                    <p className={Styles.experience_section_title}>Experience</p>
-                    <div>
-                        <Image src={'/images/logo/magi_logo.png'} width={150} height={150} objectFit="fill"/>
-                        <p className={Styles.experience_title}>Magi Web Production | 2020 - 2021</p>
-                        <p className={Styles.experience_position}>Co-CEO and Software Developer</p>
-                        <p className={Styles.experience_description}>A start-up company which provides web solutions to businesses</p>
-                        <a href="https://company-website-coral.vercel.app/" target="_blank" rel="noopener noreferrer" className={Styles.hyperlink}>Visit Website</a>
-                    </div>
-            </section>
-            <section className={Styles.portfolio_section}>
-                <div className={Styles.portfolio_title_container}>
-                    <p className={Styles.portfolio_title}>
-                        Portfolio
-                    </p>
-                    <p className={Styles.portfolio_subtitle}>
-                        Knowledge and skills
-                    </p>
-                </div>
-                <div className={Styles.portfolio_grid}>
-                    <a className={Styles.portfolio_grid_item} href="https://github.com/codinghim/team_white_308_thecomicverse" target="_blank" rel="noopener noreferrer">
-                        <div className={Styles.portfolio_image_container}>
-                            <Image src={`/images/comic_thumbnail.jpg`} width={350} height={250} objectFit="fill"/>
-                            <div className={Styles.portfolio_grid_item_hover}>
-                                <div className={Styles.portfolio_grid_item_hover_title}>
-                                    View
-                                </div>
-                            </div>
-                        </div>
-                        <div className={Styles.portfolio_image_title_container}>
-                            <p className={Styles.portfolio_image_title}>The ComicVerse</p>
-                            <p className={Styles.portfolio_image_subtitle}>Project Description</p>
-                        </div>
-                        
-                    </a>
-                    <a className={Styles.portfolio_grid_item} href="https://github.com/codinghim/company_website" target="_blank" rel="noopener noreferrer">
-                        <div className={Styles.portfolio_image_container}>
-                            <Image src={`/images/magi_thumbnail.png`} width={350} height={250} objectFit="fill"/>
-                            <div className={Styles.portfolio_grid_item_hover}>
-                                <div className={Styles.portfolio_grid_item_hover_title}>
-                                    View
-                                </div>
-                            </div>
-                        </div>
-                        <div className={Styles.portfolio_image_title_container}>
-                            <p className={Styles.portfolio_image_title}>Magi Website</p>
-                            <p className={Styles.portfolio_image_subtitle}>project and description</p>
-                        </div>
-                    </a>
-                    <a className={Styles.portfolio_grid_item} href="https://github.com/codinghim/company_website" target="_blank" rel="noopener noreferrer">
-                        <div className={Styles.portfolio_image_container}>
-                            <Image src={`/images/cyber_security_thumbnail.jpg`} width={350} height={250} objectFit="fill"/>
-                            <div className={Styles.portfolio_grid_item_hover}>
-                                <div className={Styles.portfolio_grid_item_hover_title}>
-                                    View
-                                </div>
-                            </div>
-                        </div>
-                        <div className={Styles.portfolio_image_title_container}>
-                            <p className={Styles.portfolio_image_title}>Website Templates</p>
-                            <p className={Styles.portfolio_image_subtitle}>project and description</p>
-                        </div>
-                    </a>
-                    <a className={Styles.portfolio_grid_item} href="https://github.com/codinghim/company_website" target="_blank" rel="noopener noreferrer">
-                        <div className={Styles.portfolio_image_container}>
-                            <Image src={`/images/cyber_security_thumbnail.jpg`} width={350} height={250} objectFit="fill"/>
-                            <div className={Styles.portfolio_grid_item_hover}>
-                                <div className={Styles.portfolio_grid_item_hover_title}>
-                                    View
-                                </div>
-                            </div>
-                        </div>
-                        <div className={Styles.portfolio_image_title_container}>
-                            <p className={Styles.portfolio_image_title}>Flash Cards</p>
-                            <p className={Styles.portfolio_image_subtitle}>project and description</p>
-                        </div>
-                    </a>
-                    <a className={Styles.portfolio_grid_item} href="https://github.com/codinghim/CSE363-Cyber-Security-Attacks" target="_blank" rel="noopener noreferrer">
-                        <div className={Styles.portfolio_image_container}>
-                            <Image src={`/images/cyber_security_thumbnail.jpg`} width={350} height={250} objectFit="fill"/>
-                            <div className={Styles.portfolio_grid_item_hover}>
-                                <div className={Styles.portfolio_grid_item_hover_title}>
-                                    View
-                                </div>
-                            </div>
-                        </div>
-                        <div className={Styles.portfolio_image_title_container}>
-                            <p className={Styles.portfolio_image_title}>Cyber Security Attacks</p>
-                            <p className={Styles.portfolio_image_subtitle}>project and description</p>
-                        </div>
-                    </a>
-                </div>
-            </section>
+            
         </Layout>
         </>
   )
